23c860b4be966c5366be5e5248188d2a67c36355
Joey Dreijer <jdreijer@specterops.io>
2026-05-07 15:52:16 +0200

    Added query bundle generation as CLI command

--- src/openhound/cli/saved_search.py
+++ src/openhound/cli/saved_search.py
@@ -4,6 +4,7 @@ from typing import Annotated
 
 import typer
 
+from openhound.core.models.saved_search import QueryBundle
 from openhound.core.progress import Progress
 from openhound.core.saved_searches import SavedSearches, Strategy
 
@@ -15,30 +16,35 @@ class Format(str, Enum):
     yaml = "yaml"
 
 
+class OutputFormat(str, Enum):
+    json = "json"
+    zip = "yaml"
+
+
 @saved_searches.command(help="Upload saved searches to BloodHound")
 def upload(
-    path: Annotated[
-        Path,
-        typer.Argument(
-            exists=True,
-            file_okay=False,
-            dir_okay=True,
-            readable=True,
-            resolve_path=True,
-            help="Directory where saved searches are located",
+        path: Annotated[
+            Path,
+            typer.Argument(
+                exists=True,
+                file_okay=False,
+                dir_okay=True,
+                readable=True,
+                resolve_path=True,
+                help="Directory where saved searches are located",
+            ),
+        ],
+        file_format: Format = typer.Option(
+            default=Format.json,
+            help="File format of the saved searches (json or yaml)",
         ),
-    ],
-    file_format: Format = typer.Option(
-        default=Format.json,
-        help="File format of the saved searches (json or yaml)",
-    ),
-    strategy: Strategy = typer.Option(
-        default=Strategy.skip,
-        help="Skip or overwrite saved search if the name already exists",
-    ),
-    progress: Progress = typer.Option(
-        Progress.tqdm, help="Select progress tracker option"
-    ),
+        strategy: Strategy = typer.Option(
+            default=Strategy.skip,
+            help="Skip or overwrite saved search if the name already exists",
+        ),
+        progress: Progress = typer.Option(
+            Progress.tqdm, help="Select progress tracker option"
+        ),
 ):
     search_files = (
         path.rglob("**/*.json")
@@ -48,3 +54,36 @@ def upload(
     pipeline = SavedSearches(progress=progress, strategy=strategy)
     results = pipeline.run(list(search_files), file_format=file_format)
     return results
+
+
+@saved_searches.command(help="Create a single saved searches json/zip bundle")
+def bundle(
+        path: Annotated[
+            Path,
+            typer.Argument(
+                exists=True,
+                file_okay=False,
+                dir_okay=True,
+                readable=True,
+                resolve_path=True,
+                help="Directory where saved searches are located",
+            ),
+        ],
+        output_path: Annotated[typer.FileTextWrite, typer.Argument()],
+        file_format: Format = typer.Option(
+            default=Format.json,
+            help="File format of the saved searches (json or yaml)",
+        ),
+        output_format: OutputFormat = typer.Option(
+            default=Format.json,
+            help="File format for the saved searches bundle (json or zip)",
+        )
+):
+    search_files = (
+        path.rglob("**/*.json")
+        if file_format == Format.json
+        else path.rglob("**/*.yaml")
+    )
+
+    bundle_object = QueryBundle.from_paths(list(search_files), file_format=file_format)
+    bundle_object.save(output_path, output_format=output_format)
--- src/openhound/core/models/saved_search.py
+++ src/openhound/core/models/saved_search.py
@@ -1,9 +1,22 @@
 import json
+import zipfile
+from enum import Enum
+from io import TextIOWrapper
 from pathlib import Path
 from typing import Optional, Union
 
 from pydantic import BaseModel, ConfigDict, field_validator
 from yaml import safe_load
+
+
+class Format(str, Enum):
+    json = "json"
+    yaml = "yaml"
+
+
+class OutputFormat(str, Enum):
+    json = "json"
+    zip = "yaml"
 
 
 class SavedSearch(BaseModel):
@@ -14,7 +27,7 @@ class SavedSearch(BaseModel):
     query: str
 
     @classmethod
-    def from_json(cls, file_path: Path) -> "SavedSearch":
+    def from_file(cls, file_path: Path) -> "SavedSearch":
         with open(file_path, "r") as file_object:
             json_object = json.loads(file_object.read())
         return cls(**json_object)
@@ -52,7 +65,63 @@ class SavedSearchExtended(SavedSearch):
         return value if isinstance(value, list) else [value]
 
     @classmethod
-    def from_yaml(cls, file_path: Path) -> "SavedSearchExtended":
+    def from_file(cls, file_path: Path) -> "SavedSearchExtended":
         with open(file_path, "r") as file_object:
             yaml_object = safe_load(file_object.read())
         return cls(**yaml_object)
+
+
+class QueryBundle:
+    def __init__(self, queries: list[SavedSearchExtended | SavedSearch], file_format: Format = Format.json) -> None:
+        self.queries = queries
+        self.file_format = file_format
+
+    # @staticmethod
+    # def load_query(cypher_query: Path,
+    #                input_format: Literal['yml', 'json'] = 'json') -> ExtendedCypherQuery | CypherQuery:
+    #     model_choices = {
+    #         'yml': SavedSearchExtended,
+    #         'json': SavedSearch,
+    #     }
+    #     cypher_model = model_choices[input_format]
+    #     with open(cypher_query, "r") as query_file:
+    #         yaml_obj = safe_load(query_file) if input_format == "yml" else json.load(query_file)
+    #         return cypher_model(**yaml_obj)
+
+    @classmethod
+    def from_paths(cls, all_files: list[Path], file_format: Format = Format.json) -> "QueryBundle":
+        # cypher_queries = list(input_dir.rglob(f"*.{input_format}"))
+
+        model_choices = {
+            'yaml': SavedSearchExtended,
+            'json': SavedSearch,
+        }
+
+        queries = [
+            model_choices[file_format].from_file(cypher_query) for cypher_query in
+            all_files
+        ]
+        return cls(queries, file_format)
+
+    def _to_json(self, output_file: TextIOWrapper) -> None:
+        all_objects = [query.model_dump() for query in self.queries]
+        output_file.write(json.dumps(all_objects, indent=2))
+
+    def _to_zip(self, output_file: TextIOWrapper) -> None:
+        with zipfile.ZipFile(
+                file=output_file.name,
+                mode="w",
+                compression=zipfile.ZIP_DEFLATED,
+                compresslevel=9,
+        ) as archive:
+            for query in self.queries:
+                archive.writestr(
+                    zinfo_or_arcname=f"{query.name}.json",
+                    data=query.model_dump_json().encode(),
+                )
+
+    def save(self, output_file: TextIOWrapper, output_format: OutputFormat = OutputFormat.json) -> None:
+        if output_format == Format.json:
+            self._to_json(output_file)
+        elif output_format == Format.yaml:
+            self._to_zip(output_file)
